231849919ee8b0eae8fa4e69764ca1a16efa87e8
aashutoshbhattfractals <aashutosh.bhatt@fractal.ai>
2025-06-16 18:52:57 +0000

    update model.pkl

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 app = Flask(__name__)
 
 # Load the model
-with open('model.pkl', 'rb') as f:
+with open('/app/model.pkl', 'rb') as f:
   model = pickle.load(f)
 
 @app.route('/')
